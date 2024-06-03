5005f1e7cfe318cace237b6e7897be8db2bcac6a
nayab829 <nayabg829@gmail.com>
2024-06-03 16:22:10 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const tabTitles = document.getElementsByClassName("tab-title");
+const tabContents = document.getElementsByClassName("tab-contents");
+const menu = document.querySelector(".nav-links");
+function openTab(event, tabname) {
+  for (let tabTitle of tabTitles) {
+    tabTitle.classList.remove("active-link");
+  }
+  for (let tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+// Responsive Navigation
+function openMenu(){
+menu.classList.toggle("active")
+}
+function closeMenu(){
+menu.classList.remove("active")
+}
